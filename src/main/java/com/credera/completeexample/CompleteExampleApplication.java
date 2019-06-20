1ce0363b7734a5c175e3cf47c1d93c147537d4ed
Casey Brooks <cjbrooks12@gmail.com>
2019-06-20 18:00:34 -0500

    Initial Commit From Spring Initializr

---
+++ src/main/java/com/credera/completeexample/CompleteExampleApplication.java
@@ -0,0 +1,13 @@
+package com.credera.completeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompleteExampleApplication.class, args);
+    }
+
+}
